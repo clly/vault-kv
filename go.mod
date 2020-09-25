c045f5c0eec4902924a8f0d8e82acfbe50c6be16
Connor Kelly <connor.r.kelly@gmail.com>
2020-09-24 22:45:41 -0500

    Add function checking if the mount path is versionized

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/hashicorp/vault/api v1.0.4
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 )
--- kv.go
+++ kv.go
@@ -1,14 +1,23 @@
 package vaultkv
 
-import "github.com/hashicorp/vault/api"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/hashicorp/vault/api"
+)
+
+const internalUIPath = "/v1/sys/internal/ui/mounts"
 
 type KV struct {
 	vaultapi *api.Client
 }
 
-func New(api *api.Client) *KV {
+func New(client *api.Client) *KV {
 	return &KV{
-		vaultapi: api,
+		vaultapi: client,
 	}
 }
 
@@ -28,6 +37,50 @@ func (k *KV) Delete(path string) (*api.Secret, error) {
 	return k.vaultapi.Logical().DeleteWithData(path, nil)
 }
 
-func (k *KV)  Raw() *api.Client {
+func (k *KV) Raw() *api.Client {
 	return k.vaultapi
+}
+
+func isVersionedKV(path string, client api.Client) (bool, error) {
+	req := client.NewRequest("GET", internalUIPath)
+	resp, err := client.RawRequest(req)
+	if err != nil {
+		return false, err
+	}
+
+	secret, err := api.ParseSecret(resp.Body)
+	if err != nil {
+		return false, err
+	}
+	secretMounts, ok := secret.Data["secret"].(map[string]interface{})
+	if !ok {
+		return false, fmt.Errorf("failed to retrieve secret mounts")
+	}
+	mountPath := getMountFromPath(path)
+	rawMount, ok := secretMounts[mountPath]
+	if !ok {
+		return false, nil
+	}
+
+	mount := &api.MountOutput{}
+	err = mapstructure.Decode(rawMount, api.MountOutput{})
+	if err != nil {
+		return false, fmt.Errorf("failed to decode mount configuration %w", err)
+	}
+	v, ok := mount.Options["version"]
+	if !ok {
+		return false, nil
+	}
+	if v == "2" {
+		return true, nil
+	}
+	return false, nil
+}
+
+func getMountFromPath(path string) string {
+	idx := strings.Index(path, "/")
+	if idx == -1 {
+		return path
+	}
+	return path[:idx]
 }
